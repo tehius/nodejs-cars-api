4b78d71f336d2db04d1dd97211e3ddadc550dad2
tehius <felipe.acguedes@gmail.com>
2024-04-24 10:56:55 -0300

    linked the users.js to index.js; i can now see the mock db in the browser

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import userRoutes from './routes/users.js'
 
 const app = express();
 const PORT = 3000;
 
-app.use (bodyParser.json())
+app.use(bodyParser.json());
+
+app.use('/users', userRoutes);
 
 app.get('/', (req, res) => {
     console.log('[GET ROUTE]');
